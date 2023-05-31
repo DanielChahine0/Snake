daba1c0533576f7e685437ad8b7c780a5af9a848
DanielChahine0 <chahined@my.yorku.ca>
2023-05-31 07:39:22 -0400

    Added a theme section

--- Snake_main.py
+++ Snake_main.py
@@ -1,5 +1,3 @@
-import time
-
 import pygame
 import random
 
@@ -29,6 +27,7 @@ SNAKE = [[SNAKE_SIZE*10, SNAKE_SIZE*5],
          [SNAKE_SIZE*5, SNAKE_SIZE*5]]
 DIRECTION = "right"
 CHANGE = "right"
+DEAD = False
 SPEED = SNAKE_SIZE
 
 # Game speed
@@ -44,6 +43,14 @@ SCORE = 0
 FONT = pygame.font.SysFont("Times New Roman", 30)
 GAME_OVER_FONT = pygame.font.SysFont("Time New Roman", 60)
 
+# Settings
+SETTING = 0
+THEME_Fruit = "white"
+THEME_Snake = "yellow"
+THEME_Border = "green"
+THEME_Ground = "black"
+THEME_NAME = "Basic"
+
 
 """
 ----------------------------------------------------------------------------------
@@ -55,7 +62,7 @@ GAME_OVER_FONT = pygame.font.SysFont("Time New Roman", 60)
 # Functions that draws and generate the fruit
 def generate_fruit():
     global FRUIT
-    FRUIT = [random.randint(0, WIDTH//SNAKE_SIZE-1)*SNAKE_SIZE, random.randint(0, HEIGHT//SNAKE_SIZE-1)*SNAKE_SIZE]
+    FRUIT = [random.randint(1, WIDTH//SNAKE_SIZE-2)*SNAKE_SIZE, random.randint(1, HEIGHT//SNAKE_SIZE-2)*SNAKE_SIZE]
 
 
 def draw_fruit():
@@ -67,21 +74,22 @@ def draw_fruit():
 
     fruit_rect = pygame.Rect(FRUIT[0], FRUIT[1], SNAKE_SIZE, SNAKE_SIZE)
     # print(FRUIT)
-    pygame.draw.rect(WINDOW, "red", fruit_rect)
+    pygame.draw.rect(WINDOW, THEME_Fruit, fruit_rect)
 
 
 # Game over function
 def game_over():
-    global SPEED
+    global SPEED, DEAD
 
+    DEAD = True
     GO_Surface = GAME_OVER_FONT.render("YOUR SCORE IS: "+str(SCORE), True, "black")
     height = GO_Surface.get_height()
     width = GO_Surface.get_width()
     X = (WIDTH-width)/2
     Y = (HEIGHT-height)/2
-    margin = 50
+    margin = 25
     RED_REC = pygame.Rect(X-margin, Y-margin, width+2*margin, height+2*margin)
-    pygame.draw.rect(WINDOW, "red", RED_REC)
+    pygame.draw.rect(WINDOW, THEME_Fruit, RED_REC, 0, 10)
     WINDOW.blit(GO_Surface, (X, Y))
     pygame.display.update()
 
@@ -90,16 +98,22 @@ def game_over():
 
 # Function to display the current score of the game
 def display_score():
-    score_surface = FONT.render("Score: " + str(SCORE), True, "white")
-    WINDOW.blit(score_surface, (10, 10))
+    score_surface = FONT.render("Score: " + str(SCORE), True, THEME_Fruit)
+    margin = 10
+    WINDOW.blit(score_surface, (SNAKE_SIZE+margin, SNAKE_SIZE+margin))
+
+
+def display_theme():
+    name = FONT.render("Theme: " + str(THEME_NAME), True, THEME_Fruit)
+    WINDOW.blit(name, (WIDTH-SNAKE_SIZE-200, HEIGHT-SNAKE_SIZE-40))
 
 
 # Function to check for collision with the wall and self collision
 def check_collision():
     # Out of border mechanism
-    if not (0 <= snake_position[0] <= WIDTH-SNAKE_SIZE):
+    if not (SNAKE_SIZE <= snake_position[0] <= WIDTH-SNAKE_SIZE*2):
         game_over()
-    if not (0 <= snake_position[1] <= HEIGHT-SNAKE_SIZE):
+    if not (SNAKE_SIZE <= snake_position[1] <= HEIGHT-SNAKE_SIZE*2):
         game_over()
 
     # Self collision
@@ -110,17 +124,20 @@ def check_collision():
 
 # Drawing functions
 def draw_bg():
-    image = pygame.transform.scale(pygame.image.load("Resources/Block.png"), (SNAKE_SIZE, SNAKE_SIZE))
-    _, _, width, height = image.get_rect()
-    tiles = []
 
-    for i in range(WIDTH//width+1):
-        for j in range(HEIGHT//height+1):
-            pos = (i*width, j*height)
-            tiles.append(pos)
+    # image = pygame.transform.scale(pygame.image.load("Resources/Block.png"), (SNAKE_SIZE, SNAKE_SIZE))
+    # _, _, width, height = image.get_rect()
+    # tiles = []
+    #
+    # for i in range(WIDTH//width+1):
+    #     for j in range(HEIGHT//height+1):
+    #         pos = (i*width, j*height)
+    #         tiles.append(pos)
+    # for tile in tiles:
+    #     WINDOW.blit(image, tile)
 
-    for tile in tiles:
-        WINDOW.blit(image, tile)
+    bg = pygame.Rect(0, 0, WIDTH, HEIGHT)
+    pygame.draw.rect(WINDOW, THEME_Ground, bg)
 
 
 # function that draws the snake
@@ -153,7 +170,18 @@ def draw_snake():
     else:
         SNAKE.pop()
     for pos in SNAKE:
-        pygame.draw.rect(WINDOW, "green", pygame.Rect(pos[0], pos[1], SNAKE_SIZE, SNAKE_SIZE))
+        pygame.draw.rect(WINDOW, THEME_Snake, pygame.Rect(pos[0], pos[1], SNAKE_SIZE, SNAKE_SIZE))
+
+
+def draw_border():
+    left = pygame.Rect(0, 0, SNAKE_SIZE, HEIGHT)
+    right = pygame.Rect(WIDTH-SNAKE_SIZE, 0, SNAKE_SIZE, HEIGHT)
+    top = pygame.Rect(0, 0, WIDTH, SNAKE_SIZE)
+    bottom = pygame.Rect(0, HEIGHT-SNAKE_SIZE, WIDTH, SNAKE_SIZE)
+    pygame.draw.rect(WINDOW, THEME_Border, left)
+    pygame.draw.rect(WINDOW, THEME_Border, right)
+    pygame.draw.rect(WINDOW, THEME_Border, top)
+    pygame.draw.rect(WINDOW, THEME_Border, bottom)
 
 
 # Draw everything
@@ -161,16 +189,18 @@ def draw():
     draw_bg()
     draw_snake()
     draw_fruit()
+    draw_border()
     display_score()
+    display_theme()
 
     # Makes sure to update the display after drawing
     pygame.display.update()
 
 
 def restart():
-    global SPEED, SNAKE_SIZE, snake_position, SNAKE, DIRECTION, CHANGE, SCORE
+    global SPEED, SNAKE_SIZE, snake_position, SNAKE, DIRECTION, CHANGE, SCORE, DEAD
 
-    # Snake
+    DEAD = False
     SNAKE_SIZE = 25
     snake_position = [SNAKE_SIZE * 10, SNAKE_SIZE * 5]
     SNAKE = [[SNAKE_SIZE * 10, SNAKE_SIZE * 5],
@@ -186,12 +216,57 @@ def restart():
     generate_fruit()
 
 
+def theme(number):
+    global THEME_Border, THEME_Fruit, THEME_Snake, THEME_Ground, THEME_NAME
+
+    # Normal
+    if number == 0:
+        THEME_Snake = "white"
+        THEME_Fruit = "white"
+        THEME_Ground = "black"
+        THEME_Border = "white"
+        THEME_NAME = "Basic"
+
+    # Desert
+    if number == 1:
+        THEME_Snake = (252, 186, 3)
+        THEME_Fruit = (252, 132, 3)
+        THEME_Ground = (179, 99, 14)
+        THEME_Border = (105, 63, 19)
+        THEME_NAME = "Desert"
+
+    # Snow
+    if number == 2:
+        THEME_Snake = (72, 193, 219)
+        THEME_Fruit = (200, 241, 250)
+        THEME_Ground = (125, 231, 255)
+        THEME_Border = (22, 79, 92)
+        THEME_NAME = "Snow"
+
+    # Space
+    if number == 3:
+        THEME_Snake = (6, 19, 87)
+        THEME_Fruit = (234, 242, 7)
+        THEME_Ground = (13, 29, 110)
+        THEME_Border = (7, 13, 43)
+        THEME_NAME = "Space"
+
+    # Jungle
+    if number == 4:
+        THEME_Snake = (88, 173, 9)
+        THEME_Fruit = (255, 0, 0)
+        THEME_Ground = (19, 102, 4)
+        THEME_Border = (87, 54, 12)
+        THEME_NAME = "Jungle"
+
+
 # Main function
 def main():
     # global variables
     global CHANGE, Eat
 
     running = True
+    theme(0)
 
     # main loop
     while running:
@@ -213,8 +288,18 @@ def main():
                     CHANGE = "right"
                 if event.key == pygame.K_t:
                     Eat = True
-                if event.key == pygame.K_r:
+                if event.key == pygame.K_SPACE and DEAD:
                     restart()
+                if event.key == pygame.K_KP1:
+                    theme(1)
+                if event.key == pygame.K_KP0:
+                    theme(0)
+                if event.key == pygame.K_KP2:
+                    theme(2)
+                if event.key == pygame.K_KP3:
+                    theme(3)
+                if event.key == pygame.K_KP4:
+                    theme(4)
 
         # Draw everything
         draw()
